f8fc1c039323172a9967dc69591258f758af5d26
John P Weldon <85202691+jpweldon@users.noreply.github.com>
2021-07-04 17:49:42 -0500

    Add Pants Selection to clothes_picker Function

--- CLI_Practice/cli.py
+++ CLI_Practice/cli.py
@@ -4,8 +4,13 @@
 import fire
 import random
 
-def clothes_picker():
+def clothes_picker(pants=False):
     shirts_list = ["solid blue", "red striped","purple and green tie dye", "black dress shirt"]
+
+    pants_list = ["Black dress pants", "Gray sweatpants", "Khakis"]
+
+    if pants:
+        return random.choice(shirts_list), random.choice(pants_list)
 
     return random.choice(shirts_list)
 
