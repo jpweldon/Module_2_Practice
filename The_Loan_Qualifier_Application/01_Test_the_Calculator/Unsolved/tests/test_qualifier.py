6e7fa281409d292e20f0413d04faf36b4ca3207d
John P Weldon <85202691+jpweldon@users.noreply.github.com>
2021-07-04 22:16:26 -0500

    Add Test for Loan-to-Value Ratio to The_Loan_Qualifier_Application/01_Test_the_Calculator

--- The_Loan_Qualifier_Application/01_Test_the_Calculator/Unsolved/tests/test_qualifier.py
+++ The_Loan_Qualifier_Application/01_Test_the_Calculator/Unsolved/tests/test_qualifier.py
@@ -11,4 +11,4 @@ def test_calculate_monthly_debt_ratio():
     assert calculators.calculate_monthly_debt_ratio(1500, 4000) == 0.375
 
 def test_calculate_loan_to_value_ratio():
-    # @TODO: Your code here!
+    assert calculators.calculate_loan_to_value_ratio(210000, 250000) == 0.84
