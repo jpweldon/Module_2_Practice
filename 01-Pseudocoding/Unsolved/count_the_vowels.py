3e15b024dec0c0bd60b42c148313d0cf4212d1e8
John P Weldon <85202691+jpweldon@users.noreply.github.com>
2021-07-10 18:51:17 -0500

    Update count_the_vowels.txt and Add count_the_vowels.py in 01-Pseudocoding

--- 01-Pseudocoding/Unsolved/count_the_vowels.py
+++ 01-Pseudocoding/Unsolved/count_the_vowels.py
@@ -1,7 +1,11 @@
 def count_the_vowels(str):
   # initialize a counter variable to 0
+  num_vowels = 0
   # loop through each character in the string
+  for char in str:
     # if the char matches one of the vowels in the uppercase of lowercase
-      # increment counter by one
+    if char in "aeiouAEIOU":
+        # increment counter by one
+        num_vowels = num_vowels + 1
   # return the counter
-  
+  return num_vowels
